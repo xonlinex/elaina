39b79f2cd924e779d39cf13b4312afe2cf979afd
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-07-31 18:13:26 -0400

    modify: flake zen-browser

--- flake.nix
+++ flake.nix
@@ -17,10 +17,10 @@
         nixpkgs.follows = "nixpkgs";
         home-manager.follows = "home-manager";
       };
-      firefox-addons = {
-        url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
