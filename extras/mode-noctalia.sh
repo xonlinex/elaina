694ca6b5fe6e06b07005f153045c274871b25b01
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-08-05 12:43:14 -0400

    fix(extras): update noctalia mode script

--- extras/mode-noctalia.sh
+++ extras/mode-noctalia.sh
@@ -3,8 +3,4 @@
 # Using noctalia
 current_mode=$(noctalia msg theme-mode-get)
 
-if [ "$current_mode" = "light" ]; then
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-else
-  gsettings set org.gnome.desktop.interface color-scheme 'default'
-fi
+gsettings set org.gnome.desktop.interface color-scheme "prefer-$current_mode"
