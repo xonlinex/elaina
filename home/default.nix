8060e0d653f301a1ccfe0c36dc9dffdc3d2bca96
xOnlinEx <daniel.floresvargas666@gmail.com>
2026-08-05 12:42:48 -0400

    refactor(home): entry point module restructuring

---
+++ home/default.nix
@@ -0,0 +1,14 @@
+{ inputs, pkgs, ... }: {
+  home.username = "xonlinex";
+  home.homeDirectory = "/home/xonlinex";
+  home.stateVersion = "26.05";
+
+  imports = [
+    (inputs.import-tree ./packages)
+    (inputs.import-tree ./fonts)
+    ./terminal
+    ./editors/editorconfig.nix
+  ];
+
+  programs.home-manager.enable = true;
+}
